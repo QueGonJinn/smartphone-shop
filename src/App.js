af79b49bb25b6e53a0ce53c4d05d4c59b85b939a
Yury Molaikow <molialow13@gmail.com>
2022-12-20 17:02:05 +0300

    feat: creating the structure of the application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import './scss/app.scss';
 import Header from './components/Header';
-
-function Category() {
-	return (
-		<div class="categories">
-			<ul>
-				<li class="active">Все</li>
-				<li>Asus</li>
-				<li>MSI</li>
-				<li>Acer</li>
-				<li>HP</li>
-				<li>Aplle</li>
-			</ul>
-		</div>
-	);
-}
+import Category from './components/Category';
+import Sort from './components/Sort';
+import LaptopBlock from './components/LaptopBlock';
 
 function App() {
 	return (
-		<div>
+		<div className="wrapper">
 			<Header />
-			<Category />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Category />
+						<Sort />
+					</div>
+					<h2 className="content__title">All Laptop</h2>
+					<div className="content__items">
+						<LaptopBlock
+							price={1200}
+							title="Huawei MateBook D 15"
+							img="https://content2.onliner.by/catalog/device/main/56587377c4b9d5de1b73f521f42e40ce.jpeg"
+						/>
+						<LaptopBlock
+							price={1500}
+							title="Apple Macbook Air 13"
+							img="https://content2.onliner.by/catalog/device/main/dced3c38dbb7067687bb1f766396eeb0.jpeg"
+						/>
+						<LaptopBlock
+							price={1600}
+							title="ASUS ROG Strix G15"
+							img="https://content2.onliner.by/catalog/device/main/3db1d78bf0a7735b064fa2dd0882fa84.jpeg"
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Category.jsx
@@ -0,0 +1,16 @@
+function Category() {
+	return (
+		<div className="categories">
+			<ul>
+				<li className="active">Все</li>
+				<li>Asus</li>
+				<li>MSI</li>
+				<li>Acer</li>
+				<li>HP</li>
+				<li>Aplle</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Category;
